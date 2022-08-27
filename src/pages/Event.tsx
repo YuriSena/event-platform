a0e3171c1dcbc9f76168b132919e1501e4e97ffc
YuriSena <yuriborgessena@gmail.com>
2022-08-27 00:18:14 -0300

    Apollo and Router config

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,13 +1,18 @@
+import { useParams } from "react-router-dom";
+import { EventPlaceholder } from "../components/EventPlaceholder";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 import { Video } from "../components/Video";
 
 export function Event() {
+  const { slug } = useParams<{ slug: string }>();
+  console.log(slug + " slug");
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex flex-1">
-        <Video />
+        { slug ? <Video /> : <EventPlaceholder />}
         <Sidebar />
       </main>
     </div>
